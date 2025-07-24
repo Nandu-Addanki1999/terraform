3390a47e0ec2411c9faf714d99e0eb625cb03481
Nandu-Addanki1999 <nandua1999@gmail.com>
2025-07-24 21:42:22 +0530

    terraform

--- import/ec2.tf
+++ import/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "linux" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-07f2c9320870ade43"]
+  vpc_security_group_ids = ["sg-067ee546f9d105fbd"]
 
   tags = {
     Name = "linux-change"
